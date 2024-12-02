6b9098af3c270504eb0108c3a9ad2a99c6d6ed5d
SrinivasRapelli <123249580+SrinivasRapelli@users.noreply.github.com>
2024-12-02 19:46:00 +0530

    Update example1.java
    added 2 lines of code

--- src/test/java/examples/example1.java
+++ src/test/java/examples/example1.java
@@ -5,5 +5,8 @@ public class example1 {
 	public static void main(String[] args) {
 		System.out.println("Creating my first branch");
 		System.out.println("merging this to main branch");
+		
+		System.out.println("Creating my first branch");
+		System.out.println("merging this to main branch");
 	}
 }
