c75fa07b7cd25e539064a14475e9269bdd26eb49
SrinivasRapelli <123249580+SrinivasRapelli@users.noreply.github.com>
2024-12-02 20:57:25 +0530

    Update registerclass.java

--- src/test/java/example2/registerclass.java
+++ src/test/java/example2/registerclass.java
@@ -2,5 +2,7 @@ package example2;
 public class registerclass {
  public static void main(String[] args) {
 	System.out.println("register class");
+	 System.out.println("register class");
+	 System.out.println("register class");
 }
 }
