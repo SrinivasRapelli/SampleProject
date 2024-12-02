a9a2ea8b05252a6d6daf31c09cd84d05bc2fa1f8
Srinivas Rapelli <srini.rapelli@imperialit.co.uk>
2024-12-02 19:30:10 +0530

    added a line

--- src/test/java/examples/example1.java
+++ src/test/java/examples/example1.java
@@ -5,7 +5,5 @@ public class example1 {
 	public static void main(String[] args) {
 		System.out.println("Creating my first branch");
 		System.out.println("merging this to main branch");
-        	System.out.println("changes in master branch");
-        	
 	}
 }
