a1a0062d604e77b1374fe8b57178e6e02a151ac2
SrinivasRapelli <srinivasrapelli1387@gmail.com>
2024-12-02 20:38:20 +0530

    removed a line in class2

--- src/test/java/example2/class2.java
+++ src/test/java/example2/class2.java
@@ -3,7 +3,7 @@ package example2;
 public class class2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!!");
+		
   	System.out.println("Hello world!!");
   System.out.println("Hello world!!");
   System.out.println("hello world 4");
