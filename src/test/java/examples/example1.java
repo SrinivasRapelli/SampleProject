f35c4c1d4625e27c35927c45377d76f2e9967446
SrinivasRapelli <123249580+SrinivasRapelli@users.noreply.github.com>
2024-12-02 15:49:25 +0530

    Update example1.java
    merging to main branch

--- src/test/java/examples/example1.java
+++ src/test/java/examples/example1.java
@@ -4,5 +4,7 @@ public class example1 {
 
 	public static void main(String[] args) {
 		System.out.println("Creating my first branch");
+		System.out.println("merging this to main branch");
+		
 	}
 }
