b6a8aed555cc4d253b2207650a2d0f94b2ad3220
SrinivasRapelli <srinivasrapelli1387@gmail.com>
2024-12-02 20:37:14 +0530

    added a line in class2

--- src/test/java/example2/class2.java
+++ src/test/java/example2/class2.java
@@ -6,6 +6,7 @@ public class class2 {
 		System.out.println("Hello world!!");
   	System.out.println("Hello world!!");
   System.out.println("Hello world!!");
+  System.out.println("hello world 4");
 	}
 
 }
